74ac4fb92c7c5ef2afe33b943a690745f132bb4d
Vadim Panteleev <vadim.panteleev@icloud.com>
2025-12-03 18:10:47 +0300

    Add solutions for days 1-3

---
+++ Day1/1_part2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+int main() {
+    std::ifstream inputFile("1_input.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Open file error" << std::endl;
+        return 1;
+    }
+    int pos = 50;
+    int ans = 0;
+    std::string act;
+    while (inputFile >> act) {
+        int cnt = std::stoi(act.substr(1));
+        for (int i = 0; i < cnt; ++i) {
+            if (act[0] == 'L') {
+                pos = (pos - 1 + 100) % 100;
+            } else {
+                pos = (pos + 1) % 100;
+            }
+            if (pos == 0) {
+                ans++;
+            }
+        }
+    }
+    std::cout << ans;
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Advent of Code 2025 — C++ Solutions
+
+This repository contains my C++ solutions for **Advent of Code 2025**.
+
+## Structure
+
+* Separate folder for each day
+* Every folder includes:
+
+  * Solution for part 1
+  * Solution for part 2
+  * Input file
+
+Happy 2026 and happy coding!
