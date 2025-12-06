bca8d8bba0463a795cab13e57907144f9946f007
Vadim Panteleev <vadim.panteleev@icloud.com>
2025-12-06 14:20:25 +0300

    rename directories and files

---
+++ Day01/01_part1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+int main() {
+    std::ifstream inputFile("01_input.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Open file error" << std::endl;
+        return 1;
+    }
+    int pos = 50;
+    int ans = 0;
+    std::string act;
+    while (inputFile >> act) {
+        int amt = stoi(act.substr(1));
+        if (act[0] == 'L') {
+            pos = (pos - amt) % 100;
+        } else {
+            pos = (pos + amt) % 100;
+        }
+        if (pos == 0) {
+            ans++;
+        }
+    }
+    std::cout << ans;
+}
---
+++ Day05/05_part2.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+int main() {
+    std::ifstream input("05_input.txt");
+    if (!input.is_open()) {
+        std::cerr << "Open file error" << std::endl;
+        return 1;
+    }
+
+    struct Point {
+        long long pos, type;
+
+        auto operator<=>(const Point &) const = default;
+    };
+    std::vector<Point> points;
+    std::string s;
+    while (input >> s) {
+        int pos = s.find('-');
+        if (pos != std::string::npos) {
+            long long start = stoll(s.substr(0, pos));
+            long long end = stoll(s.substr(pos + 1));
+            points.emplace_back(start, -1);
+            points.emplace_back(end, 1);
+        } else {
+            continue;
+        }
+    }
+    std::ranges::sort(points);
+    int in_interval = 0;
+    long long ans = 0;
+    long long start = 0;
+    for (const auto &p: points) {
+        if (p.type == -1) {
+            if (!in_interval) {
+                start = p.pos;
+            }
+            in_interval++;
+        } else if (p.type == 1) {
+            in_interval--;
+            if (!in_interval) {
+                ans += p.pos - start + 1;
+            }
+        }
+    }
+    std::cout << ans;
+}
